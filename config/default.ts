511af71a16de7fdc7e874a98b1494ffa866c0332
Abdirahman <57835339+Butugey12@users.noreply.github.com>
2023-02-13 15:18:05 +0200

    feat: add default.ts

---
+++ config/default.ts
@@ -0,0 +1,9 @@
+export default {
+    port: 1337,
+    dbUri:"mongodb+srv://ifiiri:ifiiri123@cluster0.a1dq77v.mongodb.net/?retryWrites=true&w=majority",
+    apiUrl: "http:localhost",
+    saltWorkFactor: 10,
+    privateKey: "",
+    publicKey: "",
+  };
+  
