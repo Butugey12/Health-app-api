318db847682536b923cc855648370c77dd0b65aa
Abdirahman <57835339+Butugey12@users.noreply.github.com>
2023-02-16 09:44:46 +0200

    fix: update patient model

--- src/core/models/patient.model.ts
+++ src/core/models/patient.model.ts
@@ -1,11 +1,10 @@
-import { Address } from './address.model';
-import { ContactDetails } from './contact-details.model';
+import { Address } from "./address.model";
+import { ContactDetails } from "./contact-details.model";
 
 export interface Patient {
-    idNumber?: string;
-    name: string;
-    email: string;
-    password?: string;
-    contactDetails?: ContactDetails;
-    address?: Address;
+  idNumber?: string;
+  name: string;
+  email: string;
+  contactDetails?: ContactDetails;
+  address?: Address;
 }
