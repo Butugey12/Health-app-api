f72523bb003f51559c94bdefa62f16ad1c0b3bd1
Abdirahman <57835339+Butugey12@users.noreply.github.com>
2023-02-16 09:45:29 +0200

    fix: update patient model

--- src/models/patient.model.ts
+++ src/models/patient.model.ts
@@ -1,12 +1,9 @@
-import bcrypt from "bcrypt";
-import config from "config";
-import mongoose from "mongoose";
-import { Patient } from "../core/models";
+import mongoose from 'mongoose';
+import { Patient } from '../core/models';
 
 export interface patientDocument extends Patient, mongoose.Document {
   createdAt: Date;
   updatedAt: Date;
-  comparePassword(patientPassword: string): Promise<Boolean>;
 }
 
 const patientSchema = new mongoose.Schema(
@@ -14,7 +11,6 @@ const patientSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     idNumber: { type: Number, required: false },
     name: { type: String, required: true },
-    password: { type: String, required: false },
     contactDetails: {
       email: { type: String, require: false },
       phoneNumber: { type: String, require: false },
@@ -29,38 +25,11 @@ const patientSchema = new mongoose.Schema(
       province: { type: String, require: true },
       postalCode: { type: String, require: true },
     },
+    
   },
   {
     timestamps: true,
   }
 );
 
-patientSchema.pre("save", async function (next: any) {
-  let patient = this as patientDocument;
-
-  if (!patient.isModified("password")) {
-    return next();
-  }
-
-  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
-  const hash = await bcrypt.hashSync(patient.password!, salt);
-
-  patient.password = hash;
-
-  return next();
-});
-
-patientSchema.methods.comparePassword = async function (
-  inputPassword: string
-): Promise<Boolean> {
-  let patient = this as patientDocument;
-
-  return bcrypt
-    .compare(inputPassword, patient.password!)
-    .catch((e: any) => false);
-};
-
-export const patientModel = mongoose.model<patientDocument>(
-  "patient",
-  patientSchema
-);
+export const patientModel = mongoose.model<patientDocument>("Patient",patientSchema);
